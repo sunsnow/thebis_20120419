cad71af37928c2c9a26a9904dcbce2663c3fa63e
Stan Melching <0794792@gmail.com>
2012-04-26 01:05:03 +0200

    Signed-off-by: Stan Melching <0794792@gmail.com>

--- app/code/community/Pollence/Easyexploded/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Pollence_Easyexploded_Helper_Data extends Mage_Core_Helper_Abstract
-{
-
-}
--- app/code/community/TM/EasyLightbox/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class TM_EasyLightbox_Helper_Data extends Mage_Core_Helper_Abstract
-{
-    
-}
